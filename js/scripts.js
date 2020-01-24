20e832e6d4793dc89683de30d2b9abb56d3c08a8
Dusty <dustymccord@gmail.com>
2020-01-24 10:15:12 -0800

    wire up the submit button to fire the buildList funciton and print the result to the DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
   
-  function buildList(count) {
+  function buildList(count, name) {
     var returnedNumbers = [];
     var userInput = count;
+    var halString;
+
+    //validate username exists. 
+    if(name){
+      halString = "I'm sorry, "+name+". I'm afraid I can't do that.";
+    } else {
+      halString = "I'm sorry, Dave. I'm afraid I can't do that."
+    }
   
     for (let i = 0; i <= userInput; i++) {
       
       var index = Array.from(i.toString());
 
       if (index.includes('3')) {
-        returnedNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
+        returnedNumbers.push(halString);
       } else if (index.includes('2'))  {
         returnedNumbers.push("Boop!");
       } else if (index.includes('1')) {
@@ -22,9 +30,24 @@ $(document).ready(function(){
     return returnedNumbers;
   } //end build list
 
+  function printToDOM(array) {
+    $('#output').empty();
+    var front = "<div class='card'>";
+    var back = "</div>"
+    array.forEach(item => $('#output').append(front+item+back));
+  }
+
   $('form').submit(function(event){
     event.preventDefault();
-    console.log('click');
+
+    //get user number
+    var userCount = $('#number-count').val();
+
+    //get username
+    var userName = $('#user-name').val();
+
+    printToDOM(buildList(userCount, userName));
+    console.log(buildList(userCount, userName));
   });
 
 });
