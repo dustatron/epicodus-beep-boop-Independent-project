8c19fb5e9b0b96ee7563169e7740221b065d0313
Dusty <dustymccord@gmail.com>
2020-01-24 09:30:11 -0800

    second and third spec have been met. Program will replace 2 and 3 with provided text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
   var returnedNumbers = [];
-  var userInput = 10;
+  var userInput = 30;
 
   for (let i = 0; i <= userInput; i++) {
-    returnedNumbers.push(i);
+    var index = Array.from(i);
+    if (i === 1){
+      returnedNumbers.push("Beep!");
+    } else if (i === 2)  {
+      returnedNumbers.push("Boop!");
+    } else if (i === 3) {
+      returnedNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else {
+      returnedNumbers.push(i);
+    }
   }
 
 
