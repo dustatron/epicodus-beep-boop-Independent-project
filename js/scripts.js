f8f5b38a0df2d31ce6e5d976751cd9f697756c51
Dusty <dustymccord@gmail.com>
2020-01-24 10:01:28 -0800

    will add an output section to index.html. It will update the js file to  wrap the list creation in its own funciton and wire up the submit button to console.log when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
-  var returnedNumbers = [];
-  var userInput = 100;
+  
+  function buildList(count) {
+    var returnedNumbers = [];
+    var userInput = count;
+  
+    for (let i = 0; i <= userInput; i++) {
+      
+      var index = Array.from(i.toString());
 
-  for (let i = 0; i <= userInput; i++) {
-    var index = Array.from(i.toString());
+      if (index.includes('3')) {
+        returnedNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      } else if (index.includes('2'))  {
+        returnedNumbers.push("Boop!");
+      } else if (index.includes('1')) {
+        returnedNumbers.push("Beep!");
+      } else {
+        returnedNumbers.push(i);
+      }
+    } // end for loop
 
-    if (index.includes('3')) {
-      returnedNumbers.push("I'm sorry, Dave. I'm afraid I can't do that.");
-    } else if (index.includes('2'))  {
-      returnedNumbers.push("Boop!");
-    } else if (index.includes('1')) {
-      returnedNumbers.push("Beep!");
-    } else {
-      returnedNumbers.push(i);
-    }
-  }
+    return returnedNumbers;
+  } //end build list
 
-  console.log(returnedNumbers)
+  $('form').submit(function(event){
+    event.preventDefault();
+    console.log('click');
+  });
+
 });
